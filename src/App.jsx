f8b9ab300d3166a999cb24fa7e200cf7542d115a
Rashawn Clarke <102108419+Rclarke910@users.noreply.github.com>
2023-03-14 05:59:29 -0400

    added hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from './componets/Header'
+import Hero from './componets/Hero'
 
 function App() {
   return (
     <div>
      <Header />
+     <main className='main'>
+      <Hero />
+     </main>
     </div>
   )
 }
---
+++ src/componets/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='hero section' id='home'>
+      <h1 className = 'hero__title'>
+        Start Your Day The Right Way
+      </h1>
+      <button className='hero__button'>Order Now</button>
+    </section>
+  )
+}
+
+export default Hero;
+
+
